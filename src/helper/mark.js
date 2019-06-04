4641d8b618c6cac93259b87d8f028d0851246fee
huangxutao <h784124804@gmail.com>
2019-06-04 11:11:51 +0800

    add some lan

--- src/helper/mark.js
+++ src/helper/mark.js
@@ -1,7 +1,6 @@
 import Marked from 'marked';
 import Prism from 'prismjs';
 import 'prismjs/components/prism-markup-templating';
-import 'prismjs/components/prism-scss';
 import 'prismjs/components/prism-c';
 import 'prismjs/components/prism-cpp';
 import 'prismjs/components/prism-php';
@@ -10,6 +9,7 @@ import 'prismjs/components/prism-java';
 import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-http';
 import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-less';
 import 'prismjs/components/prism-scss';
 import 'prismjs/components/prism-sass';
 import 'prismjs/components/prism-perl';
@@ -19,6 +19,25 @@ import 'prismjs/components/prism-wiki';
 import 'prismjs/components/prism-swift';
 import 'prismjs/components/prism-ruby';
 import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-http';
+import 'prismjs/components/prism-nginx';
+import 'prismjs/components/prism-coffeescript';
+import 'prismjs/components/prism-git';
+import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-io';
+import 'prismjs/components/prism-pure';
+import 'prismjs/components/prism-tsx';
+import 'prismjs/components/prism-regex';
+import 'prismjs/components/prism-scheme';
+import 'prismjs/components/prism-stylus';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-wiki';
+import 'prismjs/components/prism-makefile';
+import 'prismjs/components/prism-markdown';
+import 'prismjs/components/prism-matlab';
+import 'prismjs/components/prism-kotlin';
+import 'prismjs/components/prism-ejs';
+import 'prismjs/components/prism-basic';
 
 /**
  * Function mark 解析 markdown
